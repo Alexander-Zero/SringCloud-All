8ed3ff45c97349dab965ffdf93fa692d564939ad
Alexander-Zero <51397937+Alexander-Zero@users.noreply.github.com>
2020-09-21 21:57:24 +0800

    coding change to utf8

--- FeighConsumer/src/main/java/com/alex/zero/feighconsumer/test/MainController.java
+++ FeighConsumer/src/main/java/com/alex/zero/feighconsumer/test/MainController.java
@@ -3,12 +3,14 @@ package com.alex.zero.feighconsumer.test;
 import com.alex.zero.providerapi.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/feign")
+@RefreshScope
 public class MainController {
     @Autowired
     private ConsumerUser consumerUser;
